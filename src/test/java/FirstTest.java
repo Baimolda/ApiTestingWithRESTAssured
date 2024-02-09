4c35d39867899356bfbddae97f22e1f23aea7291
Admin <asetbaimolda@gmail.com>
2024-02-10 01:37:15 +0600

    Add Logging Response Request

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -6,8 +6,8 @@ import static io.restassured.RestAssured.*;
 public class FirstTest extends TestConfig {
     @Test
     public void myFirstTest(){
-        given().
+        given().log().uri().
                 when().get(SWAPI_GET_PEOPLE + "1").
-                then().statusCode(200);
+                then().log().body().statusCode(200);
     }
 }
