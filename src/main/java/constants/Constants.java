791531c73b8d35dea7143571a1254b3c168ba40d
Admin <asetbaimolda@gmail.com>
2024-02-10 13:30:13 +0600

    Parsers Xml to Json

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,18 +1,18 @@
 package constants;
 
 import static constants.Constants.Path.SWAPI_PATH;
-import static constants.Constants.Servers.JSON_PLACEHOLDER_URL;
-import static constants.Constants.Servers.SWAPI_URL;
+import static constants.Constants.Servers.*;
 
 public class Constants {
 
     public static class RunVariable {
-        public static String server = JSON_PLACEHOLDER_URL;
+        public static String server = REQUESTBIN_URL;
         public static String path = "";
     }
     public static class Servers{
         public static String SWAPI_URL = "https://swapi.dev/";
         public static String JSON_PLACEHOLDER_URL = "https://jsonplaceholder.typicode.com";
+        public static String REQUESTBIN_URL = "https://eoxhv2stdqrfepn.m.pipedream.net";
         public static String GOOGLE_PLACES_URL;
     }
     public static class Path{
@@ -28,5 +28,6 @@ public class Constants {
         public static String JSONPLACEHOLDER_GET = "comments/";
         public static String JSONPLACEHOLDER_PUT = "posts/1";
         public static String JSONPLACEHOLDER_DELETE = "posts/1";
+        public static String JSONPLACEHOLDER_POST = "posts/";
     }
 }
--- src/test/java/JsonPlaceHolderTest.java
+++ src/test/java/JsonPlaceHolderTest.java
@@ -30,5 +30,39 @@ public class JsonPlaceHolderTest extends TestConfig {
                 when().delete(JSONPLACEHOLDER_DELETE).
                 then().log().body().statusCode(200);
     }
+    @Test
+    public void PostWithJson(){
+
+        String postJsonBody = "{\n" +
+                "\t\"title\": \"foo\",\n" +
+                "\t\"body\": \"bar\",\n" +
+                "\t\"userId\": 1\n" +
+                "}";
+
+        given().body(postJsonBody).log().uri().
+                when().post(JSONPLACEHOLDER_POST).
+                then().log().body().statusCode(201);
+    }
+    @Test
+    public void PostWithXml(){
+        String postXmlBody = "\t\t\t<?xml version=\"1.0\"?>\n" +
+                "\t\t\t<Company>\n" +
+                "\t\t\t  <Employee>\n" +
+                "\t\t\t\t  <FirstName>Tanmay</FirstName>\n" +
+                "\t\t\t\t  <LastName>Patil</LastName>\n" +
+                "\t\t\t\t  <ContactNo>1234567890</ContactNo>\n" +
+                "\t\t\t\t  <Email>tanmaypatil@xyz.com</Email>\n" +
+                "\t\t\t\t  <Address>\n" +
+                "\t\t\t\t\t   <City>Bangalore</City>\n" +
+                "\t\t\t\t\t   <State>Karnataka</State>\n" +
+                "\t\t\t\t\t   <Zip>560212</Zip>\n" +
+                "\t\t\t\t  </Address>\n" +
+                "\t\t\t  </Employee>\n" +
+                "\t\t\t</Company>\n" +
+                "\t\t\t";
+        given().body(postXmlBody).log().uri().
+                when().post("").
+                then().log().body().statusCode(200);
+    }
 
 }
