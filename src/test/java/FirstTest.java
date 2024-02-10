144b4bf230b3395356112cfc2febbc63de203644
Admin <asetbaimolda@gmail.com>
2024-02-10 12:12:20 +0600

    Delete Reqeust add to test

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,6 +1,7 @@
 import config.TestConfig;
 import org.junit.Test;
 
+import static constants.Constants.Actions.JSONPLACEHOLDER_PUT;
 import static constants.Constants.Actions.SWAPI_GET_PEOPLE;
 import static io.restassured.RestAssured.*;
 public class FirstTest extends TestConfig {
--- src/test/java/JsonPlaceHolderTest.java
+++ src/test/java/JsonPlaceHolderTest.java
@@ -1,7 +1,7 @@
 import config.TestConfig;
 import org.junit.Test;
 
-import static constants.Constants.Actions.JSONPLACEHOLDER_GET;
+import static constants.Constants.Actions.*;
 import static io.restassured.RestAssured.given;
 
 public class JsonPlaceHolderTest extends TestConfig {
@@ -9,6 +9,25 @@ public class JsonPlaceHolderTest extends TestConfig {
     public void GET(){
         given().queryParam("postId", 1).log().uri().
                 when().get(JSONPLACEHOLDER_GET).
+                then().log().body().statusCode(200);
+    }
+    @Test
+    public void Put(){
+
+        String putBodyJson = "{\n" +
+                "\t\"id\": 1,\n" +
+                "\t\"title\": \"foo\",\n" +
+                "\t\"body\": \"bar\",\n" +
+                "\t\"userId\": 1\n" +
+                "}";
+        given().body(putBodyJson).log().uri().
+                when().put(JSONPLACEHOLDER_PUT).
+                then().log().body().statusCode(200);
+    }
+    @Test
+    public void Delete(){
+        given().log().uri().
+                when().delete(JSONPLACEHOLDER_DELETE).
                 then().log().body().statusCode(200);
     }
 
